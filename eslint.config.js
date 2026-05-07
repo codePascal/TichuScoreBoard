2d20e13b8c6ebec16090a94a975f272dcdc909ce
Pascal <mup0117@gmail.com>
2026-05-07 21:00:35 +0200

    Updated configs.

--- eslint.config.js
+++ eslint.config.js
@@ -17,6 +17,10 @@ module.exports = [
     },
     rules: {
       ...tsPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
     },
   },
   {
